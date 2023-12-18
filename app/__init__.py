d47b16712fd767cb53ab6e089a9c7f42fae7a01a
MuhammadRizki8 <muhammadrizki@upi.edu>
2023-12-18 16:28:30 +0700

    insyaallah hari ini deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,30 @@
 from flask import Flask 
 from flask_cors import CORS
 from .extensions import api, db, jwt
-from .user import ns_user,UserProfilePicture
+from .user import ns_user, UserProfilePicture
 from .pertanyaan import ns_pertanyaan
 from .auth import ns_auth
 from .major import ns_predict
 from .models import User
+
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/db_minatku"
+    # Cloud SQL database configuration
+    db_user = "root"
+    db_password = "minatku1234567"
+    db_name = "db_minatku"
+    db_socket_dir = "/cloudsql"
+    cloud_sql_connection_name = "minatku:asia-southeast2:dbminatku"
+    db_host = "34.101.48.255"  # Ganti dengan alamat IP publik database
+    db_port = 3306
+
+    # Configure the connection string
+    db_uri = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     app.config["JWT_SECRET_KEY"] = "thisisasecret"
+
     CORS(app)
     api.init_app(app)
     db.init_app(app)
@@ -25,4 +39,5 @@ def create_app():
     def user_lookup_callback(_jwt_header, jwt_data):
         identity = jwt_data["sub"]
         return User.query.filter_by(email=identity).first()
+
     return app
